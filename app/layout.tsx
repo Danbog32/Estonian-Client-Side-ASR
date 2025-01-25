9fd192d1c1a58b2dc5f51c7b6c05ae66949c27eb
Danbog32 <pb0961713194@gmail.com>
2025-01-25 14:58:30 +0200

    vercel speed-insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { SettingsProvider } from "../app/components/SettingsContext";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
