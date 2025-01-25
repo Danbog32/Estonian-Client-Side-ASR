05cddb8e9c61282ef4eb705591b2610f8c2d9a3e
Danbog32 <pb0961713194@gmail.com>
2025-01-25 14:52:01 +0200

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SettingsProvider } from "../app/components/SettingsContext";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
