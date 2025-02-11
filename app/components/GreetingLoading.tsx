bab71ffedf541366c8e195f2704c44e0a8f1863b
Danbog32 <pb0961713194@gmail.com>
2025-02-11 21:36:19 +0200

    fixed adaptations

--- app/components/GreetingLoading.tsx
+++ app/components/GreetingLoading.tsx
@@ -16,12 +16,12 @@ const GreetingLoading: React.FC<GreetingLoadingProps> = ({
   const translations = {
     en: {
       greeting: "Welcome!",
-      info: "We provide an easy to use live captions experience which lives right inside your browser.",
+      info: "We provide an easy to use live captions experience which lives right inside your browser",
       loading: "The model is loading, please wait.",
     },
     et: {
       greeting: "Tere tulemast!",
-      info: "Pakume lihtsat kasutajaliidest otse teie brauseris elava otsekirjelduse kogemuse jaoks.",
+      info: "Pakume lihtsat kasutajaliidest otse teie brauseris elava otsekirjelduse kogemuse jaoks",
       loading: "Mudel laeb, palun oodake.",
     },
   };
@@ -30,22 +30,30 @@ const GreetingLoading: React.FC<GreetingLoadingProps> = ({
   const currentLoadingMessage = loadingMessage || t.loading;
 
   return (
-    <div className="absolute inset-0 flex items-center justify-center">
-      <div className="flex flex-col items-center justify-center gap-4 py-10 px-6 rounded-lg shadow-lg bg-gray-700">
-        <h1 className="text-4xl font-bold text-white mb-4">{t.greeting}</h1>
-        <div>
-          <p className="text-lg text-gray-300 mb-2 text-center">{t.info}</p>
-          <p className="text-lg text-gray-300 mb-4 text-center">
+    <div className="absolute inset-0 flex flex-col items-center justify-center animate-fadeIn">
+      <div
+        className="flex flex-col items-center justify-center text-balance gap-1 sm:gap-4 bg-gray-700 
+        max-w-[85%] lg:max-w-[40%] md:max-w-[50%] sm:max-w-[70%] rounded-lg shadow-lg py-4 px-2 sm:py-8"
+      >
+        <h1 className="text-2xl font-bold text-white mb-4 text-center sm:text-4xl animate-slideInDown">
+          {t.greeting}
+        </h1>
+
+        <p className="text-medium sm:text-lg text-gray-300 mb-2 text-center max-w-[80%] animate-fadeIn">
+          {t.info}
+        </p>
+        <div className="max-w-[80%]">
+          <p className="text-medium sm:text-lg text-gray-300 mb-4 text-center animate-fadeIn">
             {currentLoadingMessage}
           </p>
-        </div>
 
-        <Progress
-          size="md"
-          isIndeterminate
-          aria-label="Loading..."
-          className="w-full max-w-md"
-        />
+          <Progress
+            size="md"
+            isIndeterminate
+            aria-label="Loading..."
+            className="w-full max-w-md"
+          />
+        </div>
       </div>
     </div>
   );
