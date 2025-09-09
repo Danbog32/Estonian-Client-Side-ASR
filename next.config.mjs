2326fee4b347c01aa26591ce34db41acecb7385a
Bohdan <pb0961713194@gmail.com>
2025-09-09 11:39:30 +0300

    implemented shared array buffer

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,28 @@ const nextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
+          { key: "Origin-Agent-Cluster", value: "?1" },
+        ],
+      },
+      {
+        source: "/onnx/:path*",
+        headers: [
+          { key: "Cross-Origin-Resource-Policy", value: "same-origin" },
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
